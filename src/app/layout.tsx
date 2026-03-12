f5b6983b78fb8a112154a666bb123bb4fabad502
wangtao1217 <x96577213@163.com>
2026-03-12 23:11:39 +0800

    Fix: Remove unsupported props from ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
-          attribute="class"
           defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
