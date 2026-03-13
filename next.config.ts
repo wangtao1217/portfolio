2bf8ff89623b193fda1590580a23d20643c4c266
wangtao1217 <x96577213@163.com>
2026-03-13 21:51:05 +0800

    fix: add basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
   images: {
     unoptimized: true,
   },
